93caac041d88f20314410e328c2440c217cdde92
jakub-bembenek-nti-johanneberg <jakub.bembenek@elev.ga.ntig.se>
2022-02-12 22:22:09 +0100

    la till nav, burgermeny och mediaquery under min fritid.
    det var för underhållning

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-// Denna fil skall innehålla JavaScript (JS)
+const nav = document.querySelector('.navLinks')
+const burger = document.querySelector('.burger')
+
+function toggleMeny() {
+
+    nav.classList.toggle('navActive')
+    burger.classList.toggle('toggle')
+
+}
+
+document.querySelector('.burger').addEventListener('click', toggleMeny)
