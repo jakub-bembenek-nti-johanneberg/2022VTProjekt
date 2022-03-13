74c5be55b18ead0c18cf00f684314b0f6a954a1f
jakub-bembenek-nti-johanneberg <jakub.bembenek@elev.ga.ntig.se>
2022-03-13 16:35:29 +0100

    cart + funktion
    började också på en till page

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 const nav = document.querySelector('.navLinks')
 const burger = document.querySelector('.burger')
+const cart = document.querySelector('.cart')
 
 function toggleMeny() {
 
     nav.classList.toggle('navActive')
     burger.classList.toggle('toggle')
-
+    cart.classList.toggle('cartUnactive')
 }
 
 document.querySelector('.burger').addEventListener('click', toggleMeny)
+
